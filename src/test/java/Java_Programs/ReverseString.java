9d4a2b4cbe70ba05021492816beb113f9f94c7c8
rameshchalawa <rameshchalawa@gmail.com>
2026-03-02 19:34:50 +0530

    38th commit

--- src/test/java/Java_Programs/ReverseString.java
+++ src/test/java/Java_Programs/ReverseString.java
@@ -4,36 +4,34 @@ public class ReverseString {
 
 	public static void main(String[] args) {
 		
-		//Approach1 
+		// Approach 1: Using char array
 		
-		String s="welcome";
-		String rev="";
-
-		char a[]=s.toCharArray();
-
-		for(int i=a.length-1;i>=0;i--)
-		{
-			rev=rev+a[i];
-		}
-
-		System.out.println("Reverse string is:"+ rev);
-		
-		
-		
-		//Approach2 - by converting string to char array type
-		
-		/*String s = "selenium";
-		
-		String rev ="";
-		
-		for(int i=s.length()-1;i>=0; i--) {
-			
-			rev = rev + s.charAt(i);
-		}
-        System.out.println("Reverse string is: "+ rev);
-        */
-			
+        String s = "welcome"; // input string
         
-	}
+        String rev = ""; // initialize reverse string
+        
+        char a[] = s.toCharArray(); // convert string to char array
+        
+        for (int i = a.length - 1; i >= 0; i--)    // loop through array in reverse
+        { 
+        	
+            rev = rev + a[i]; // append each char to reverse string
+        }
+        
+        System.out.println("Reverse string is: " + rev); // output reversed string
 
+        // Approach 2: Using charAt() method
+        
+        /*String s = "selenium"; // input string
+         
+        String rev = ""; // initialize reverse string
+        
+        for (int i = s.length() - 1; i >= 0; i--)  // loop through string in reverse
+        { 
+            rev = rev + s.charAt(i); // append each char to reverse string
+        }
+        
+        System.out.println("Reverse string is: " + rev); // output reversed string
+        */
+    }
 }
